95ddf00a748e77df588a695dc2ca619b65c808bd
Yassef AyubBoggeano <ayub.yassef@icloud.com>
2023-11-09 11:43:28 -0800

    set up mongodb connection

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import express from 'express'
+import './db'
 
 //creating server
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("<h1>Hello World, how's tricks? :-)</h1>")
-})
-//listen to some port
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.post('/', (req, res) => {
+    console.log(req.body)
+    res.json({ message: "I am listening!" });
+});
+//listen to port
 app.listen(8000, () => {
     console.log("listening");
 });
