47e390695764e4a8d430009a5d1f9f84e139474e
Yassef AyubBoggeano <ayub.yassef@icloud.com>
2023-11-10 16:30:46 -0800

    added new post route

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import express from 'express'
-import './db'
+import express from 'express';
+import './db';
+import Note from './models/note'
 
 //creating server
 const app = express();
@@ -9,8 +10,15 @@ app.use(express.urlencoded({ extended: false }));
 
 app.post('/', (req, res) => {
     console.log(req.body)
-    res.json({ message: "I am listening!" });
+    res.json({ message: "I am listening." });
 });
+
+app.post('/create', async (req, res) => {
+    const newNote = new Note({title: req.body.title, description: req.body.description});
+    await newNote.save();
+    res.json({ message: "I am listening to create." });
+});
+
 //listen to port
 app.listen(8000, () => {
     console.log("listening");
